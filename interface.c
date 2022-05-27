81f0d8b7c57f80414f4f2ed7f9af14f5949bd7ee
ENSTA <blandine.pouch@gmail.com>
2022-05-27 12:46:00 +0200

    affichage du piano

--- interface.c
+++ interface.c
@@ -1,10 +1,13 @@
-//gcc -o myprogram myprogram.c `sdl2-config --cflags --libs`
+//gcc -o interface interface.c `sdl2-config --cflags --libs`
 // j'en suis à essayer de colorer un point.
 //https://zestedesavoir.com/tutoriels/1014/utiliser-la-sdl-en-langage-c/dessiner-dans-la-fenetre/
 
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h)
 {
@@ -13,7 +16,7 @@ int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h)
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return -1;
     }
-    if(0 != SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_SHOWN, window, renderer))
+    if(0 != SDL_CreateWindowAndRenderer(w, h, 0, window, renderer))
     {
         fprintf(stderr, "Erreur SDL_CreateWindowAndRenderer : %s", SDL_GetError());
         return -1;
@@ -29,69 +32,70 @@ int main(int argc,char *argv[]){
 	SDL_Renderer* renderer=NULL;
 	int statut=EXIT_FAILURE;
 	SDL_Color orange={255,127,40,255};
-
+	SDL_Color black={0,0,0,255};
+	SDL_Color white={255,255,255,255};
 
 	//initialisation de la SDL
-	init(window, renderer,600,200);
-	/*if(0!= SDL_Init(SDL_INIT_VIDEO)){
-		fprintf(stderr, "Erreur de SDL_Init: %s\n",SDL_GetError()); //écrit dans le flux de sortie standard d'erreur un message à propos de la dernière erreur rencontrée.
-		goto Quit;
-	}
+	init(&window, &renderer,WINDOW_WIDTH,WINDOW_HEIGHT);
 
-	//création d'une fenêtre
-	window=SDL_CreateWindow("testwindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600,200, SDL_WINDOW_SHOWN);
+	/*............... C’est à partir de maintenant que ça se passe. .......................*/
+	SDL_Surface *image=NULL;
+	SDL_Texture *texture=NULL;
 
-	if (NULL==window){
-		fprintf(stderr, "Erreur SDL_CreateWindow : %s\n", SDL_GetError());
-		goto Quit;
-	}*/
+	image=SDL_LoadBMP("piano.bmp");
 
-	/*on agit sur la fenêtre ici*/
-
-	
-	/*renderer=SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	if(NULL==renderer){
-		
-		fprintf(stderr, "Erreur SDL_CreateRenderer : %s\n", SDL_GetError());
-		goto Quit;
-	}*/
-
-	/* C’est à partir de maintenant que ça se passe. */
-
-	//colorie la fenêtre en orange
-    if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a))
+	if(NULL == image)
     {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
-        goto Quit;
-    }
-    
-    if(0 != SDL_RenderClear(renderer))
-    {
-        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
         goto Quit;
     }
 
+    texture = SDL_CreateTextureFromSurface(renderer, image);
+    SDL_FreeSurface(image);
+
+    if(NULL == texture)
+    {
+        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
+        goto Quit;
+    }
+
+	SDL_Rect rectangle;
     
-	SDL_Point point={0,0};
+
+    //on la charge en mémoire
+    if (0!=SDL_QueryTexture(texture, NULL, NULL, &rectangle.w, &rectangle.h)){
+    	fprintf(stderr, "Erreur SDL_QueryTexture : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    rectangle.x= (WINDOW_WIDTH-	rectangle.w)/2;
+    rectangle.y=(WINDOW_HEIGHT - rectangle.h)/2;
+    
+    if (0!= SDL_RenderCopy(renderer, texture, NULL, &rectangle)){
+    	fprintf(stderr, "Erreur SDL_RenderCopy : %s", SDL_GetError());
+        goto Quit;
+    }
+	
+    
+
+
+    /*if(0 != SDL_RenderClear(renderer)) //efface le rendu
+    {
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }*/
 
 	
 
 	
-	SDL_Delay(2000); //la fenêtre s'affiche pendant 2s
+	//SDL_Delay(2000); //la fenêtre s'affiche pendant 2s
 	
-	/*SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderDrawPoint(renderer, 50, 50);
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderDrawPoint(renderer, 100, 100);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-	SDL_RenderDrawPoint(renderer, 150, 150);
-	SDL_RenderPresent(renderer);
-*/
 	SDL_RenderPresent(renderer);
 	SDL_Delay(3000); //et se colore en orange pendant 3s
 
 	statut=EXIT_SUCCESS;
+
+	/*...........................................................................................*/
 	
 Quit:
 
@@ -105,3 +109,40 @@ Quit:
 	SDL_Quit(); //pour quitter la SDL
 	return statut;
 }
+
+
+
+/*//colorie la fenêtre en orange
+    if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a))
+    {
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    if(0 != SDL_RenderDrawPoint(renderer, 50, 50)){
+    	fprintf(stderr, "Erreur SDL_RenderDrawPoint : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    if(0 != SDL_RenderDrawLine(renderer, 50, 50,100,100)){
+    	fprintf(stderr, "Erreur SDL_RenderDrawLine : %s", SDL_GetError());
+        goto Quit;
+    }
+
+
+    SDL_Rect rectangle;
+    rectangle.x=100;
+    rectangle.y=100;
+    rectangle.w=100;
+    rectangle.h=100;
+
+    if(0 != SDL_SetRenderDrawColor(renderer, white.r, white.g, white.b,white.a))
+    {
+        fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    if(0 != SDL_RenderFillRect(renderer, &rectangle)){
+    	fprintf(stderr, "Erreur SDL_RenderDrawLine : %s", SDL_GetError());
+        goto Quit;
+    }*/
