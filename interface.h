a21cb7dbdb622a1485642b66900ebcb38e9e9885
ENSTA <blandine.pouch@gmail.com>
2022-05-28 00:19:00 +0200

    ajout de commentaires

--- interface.h
+++ interface.h
@@ -9,7 +9,7 @@ int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
 
 int colorNote(SDL_Renderer *renderer, SDL_Rect rectangle, SDL_Color orange, int note);
 
-void pianoColor(int* note);
+void pianoColor(int* note); //prend en argument un tableau de 3 entiers. (de 1 à 7 = de do à si; de 11 à 16 = de do# à la#; 13 à ne pas utiliser !! )
 
 
 
